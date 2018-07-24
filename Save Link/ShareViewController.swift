6c0fc3b78dfe294cc5cd5d23cbd094d7fef2fd15
Hong Jiang <hjiang@leancloud.rocks>
2018-07-24 15:09:14 +0800

    :sparkles: Saving links work in the extension

--- Save Link/ShareViewController.swift
+++ Save Link/ShareViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Social
 import MobileCoreServices
+import Apollo
 
 class ShareViewController: SLComposeServiceViewController {
 
@@ -19,9 +20,8 @@ class ShareViewController: SLComposeServiceViewController {
   }
 
   override func didSelectPost() {
-    if let email = defaults?.string(forKey: "email") {
-      NSLog("User logged in as \(email)")
-      saveLink()
+    if let token = defaults?.string(forKey: "token") {
+      saveLink(token: token)
     } else {
       let alert = UIAlertController(title: "Not logged in", message: "Please log in at the +1 Link main app.",
                                     preferredStyle: .alert)
@@ -33,22 +33,49 @@ class ShareViewController: SLComposeServiceViewController {
     }
   }
 
-  func saveLink() {
+  func createApolloClient(token: String) -> ApolloClient {
+    let configuration = URLSessionConfiguration.default
+    // Add additional headers as needed
+    configuration.httpAdditionalHeaders = ["Authorization": token] // Replace `<token>`
+    let url = URL(string: "https://add1.link/graphql/")!
+
+    return ApolloClient(networkTransport: HTTPNetworkTransport(url: url, configuration: configuration))
+  }
+
+  func saveLink(token: String) {
+    var hasPendingCallback = false
     if let item = extensionContext?.inputItems.first as? NSExtensionItem {
       if let itemProvider = item.attachments?.first as? NSItemProvider {
         if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+          hasPendingCallback = true
           itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { (url, error) -> Void in
-            if let shareURL = url as? NSURL {
-              print("***************")
-              print(shareURL)
-              print("***************")
+            if let error = error {
+              self.extensionContext!.cancelRequest(withError: error)
+            } else {
+              if let shareURL = url as? NSURL {
+                let apollo = self.createApolloClient(token: token)
+                apollo.perform(mutation: SaveLinkMutation(url: shareURL.absoluteString!)) {
+                  (result, error) in
+                  if let error = error {
+                    NSLog("Login error: \(error)")
+                    self.extensionContext!.cancelRequest(withError: error)
+                  } else {
+                    self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+                  }
+                }
+              } else {
+                self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+              }
             }
           })
         }
       }
     }
-    self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+    if !hasPendingCallback {
+      self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+    }
   }
+
   override func configurationItems() -> [Any]! {
     // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
     return []
